ae07f7a28681415359666ab69c1b3f097687f271
Rafael R. de Oliveira <rafael.roberto200618@gmail.com>
2023-04-20 20:14:18 -0300

    update and finish menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,10 +2,14 @@ const menuLateral = document.querySelector('.menu');
 const overlay = document.getElementById('overlay');
 const buttonOpenMenu = document.querySelector('.button-open');
 const buttonCloseMenu = document.querySelector('.button-close');
+const menuLinks = document.querySelectorAll('.menu__link');
 
 buttonOpenMenu.addEventListener('click', openMenu);
 buttonCloseMenu.addEventListener('click', closeMenu);
-overlay.addEventListener('click', closeMenu)
+overlay.addEventListener('click', closeMenu);
+menuLinks.forEach((link) => {
+    link.addEventListener('click', closeMenu);
+})
 
 function openMenu(){
     menuLateral.classList.add('menu-open');
